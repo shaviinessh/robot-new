ad0a3a978389ecd36c6695b0d5d585aaf3d36293
shaviinessh <m-14018743@moe-dl.edu.my>
2026-09-17 08:45:32 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onGesture(Gesture.TiltLeft, function () {
+    let SHAVIINESSH_AL_SARAVANA_SEELAN = 0
+    basic.showIcon(IconNames.Heart)
+    music.ringTone(SHAVIINESSH_AL_SARAVANA_SEELAN)
+})
+input.onGesture(Gesture.TiltRight, function () {
+	
+})
+basic.showNumber(67)
+music.play(music.stringPlayable("G A E F C G B E ", 368), music.PlaybackMode.InBackground)
+basic.forever(function () {
+	
+})
